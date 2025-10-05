4c8084e05bec516e02fd62f4f262d7994bd387c4
s12217957 <s12217957@stu.najah.edu>
2025-10-05 13:21:19 +0300

    modify book class and add library class

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -12,6 +12,11 @@ public class Book {
         this.isbn = isbn;
         this.isAvailable = true;
     }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { this.isAvailable = available; }
 
 
 }
