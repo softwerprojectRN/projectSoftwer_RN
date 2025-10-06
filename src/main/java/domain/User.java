4242632321bd0a06c09796c79a86d6767e0f5667
rahaf <ishtayehrahaf@gmail.com>
2025-10-06 20:04:32 +0300

    Add testpush class with main method

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,58 @@
+package domain;
+
+public class User {
+
+    private String username;
+    private String password;
+    private int id;
+    private boolean loggedIn;
+
+    public User(String username, String password, int id) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false;
+
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public boolean login(String username, String password) {
+        if(this.username.equals(username) && this.password.equals(password)){
+            System.out.println(" Login successful, welcome " + username + "!");
+            return  true;
+        }
+        else{
+            System.out.println("Invalid username or password.");
+
+            return false;
+        }
+    }
+    public void logout() {
+        if(this.loggedIn){
+            this.loggedIn = false;
+            System.out.println("Logged out successfully.");
+        }
+        else{
+            System.out.println("You are not logged in yet.");
+
+        }
+    }
+}
